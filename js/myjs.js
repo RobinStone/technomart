536eff7374b6267fe536127c3363ba6bbb154b7b
RobinStone <vohuanrok@mail.ru>
2018-08-27 20:58:00 +0300

    created mine content

--- js/myjs.js
+++ js/myjs.js
@@ -7,3 +7,22 @@ $('#right').on('click', function(){
 $('#left').on('click', function(){
    $('.slide').toggleClass('active-slide');
 });
+
+//-------------------- SERVICES-BTN --------------------
+
+$('.services-btn').on('click', function(){
+    $('.button-item').removeClass('current-service');
+    $(this).parent().addClass('current-service');
+    
+    var par = $(this).parent();
+    var n= $(par).index();
+    $('.services-item').removeClass('current-item');
+    $('.services-list').children().eq(n).addClass('current-item');
+});
+
+
+
+
+
+
+
