274eeaac7cd5552ef03b41d831e0a1138cb51212
RobinStone <vohuanrok@mail.ru>
2018-09-03 23:04:06 +0300

    complite first page

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,53 @@
+//---------------------- POPAP ---------------------------
+
+$('#popap').on('click', function(){
+   $('.contacts-popap').toggleClass('active-popap');
+});
+
+$('#close-contacts').on('click', function(){
+   $('.contacts-popap').toggleClass('active-popap');
+});
+
+//---------------------- SHOW-HIDE MAP ------------------
+
+$('#micro-map').on('click', function(){
+   $('.map-popap').toggleClass('active-map');
+});
+
+$('#close-map').on('click', function(){
+   $('.map-popap').toggleClass('active-map');
+});
+
+//---------------------- MAP ---------------------------
+
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [59.938631, 30.323055],
+        zoom: 17
+    });
+
+    var collectionLabels = new ymaps.GeoObjectCollection({}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/map-marker.png',
+        iconImageSize: [40, 50],
+        iconImageOffset: [-28, -40]
+});
+
+    var myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
+//        balloonContentHeader: 'ROBIN',
+//        balloonContentBody: 'ул. Мицеквича д.122 кв.10',
+//        balloonContentFooter: 'Первый подъезд, третий этаж',
+//        hintContent: 'Я ЖИВУ ТУТ'
+    });
+
+    collectionLabels.add(myPlacemark);
+    myMap.geoObjects.add(collectionLabels);
+
+}
+
+
 //-------------------- SLIDER --------------------------
 
 $('#right').on('click', function(){
